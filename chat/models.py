c14787b582fc909acba84d1efc845a6a11ef844f
Your Name <you@example.com>
2026-02-17 15:54:29 -0500

    added messages model; forgot to add this last time

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class messages(models.Model):
+    recruiterIDFK = models.ForeignKey('account.recruiter', on_delete=models.CASCADE)
+    jobSeekerIDFK = models.ForeignKey('account.jobSeeker', on_delete=models.CASCADE)
+    message = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.recruiterIDFK.user.username}-{self.jobSeekerIDFK.user.username}-{self.timestamp}"
