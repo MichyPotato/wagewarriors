47ec256faea80da96192e4bfd33e35ceb85ce832
itsZling <micahgo425@gmail.com>
2026-02-19 16:39:24 -0500

    updated settings

--- wagewarriors/urls.py
+++ wagewarriors/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('account/', include('account.urls')),
     path('jobs/', include('jobs.urls')),
-    path('chat/', include('chat.urls')),
+    path('messages/', include('chat.urls')),
     path('applicants/', include('applicants.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
