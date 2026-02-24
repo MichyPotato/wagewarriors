586b8b36bc875361be4607d0784c6a031507e4f7
itsaisabelle <isabelle.d.campeau@gmail.com>
2026-02-24 17:32:29 +0000

    urls

--- account/urls.py
+++ account/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('applications/<str:username>/', views.applications, name='account.applications'),
     path('login/', views.login_view, name='account.login'),
     path('logout/', views.logout_view, name='account.logout'),
+    path('kanban/', views.kanban, name='account.kanban'),
+    path('kanban/<int:job_id>/', views.kanban, name='account.kanban_job'),
 ]
