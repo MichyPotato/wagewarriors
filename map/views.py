55d993929b76deb9dc45ff278a9bf3470158345c
itsZling <micahgo425@gmail.com>
2026-02-25 23:05:41 -0500

    Reduced cities.csv, added navBar on map page, and created different maps for Jobseekers, and Recruiters

--- map/views.py
+++ map/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Location
 
 # Create your views here.
 def index(request):
-  return render(request, 'map/index.html')
+  cities = list(Location.objects.values('city', 'state', 'country', 'latitude', 'longitude')[19700:20000])  # Get the first 100 locations
+  context = {'cities': cities}
+  return render(request, 'map/index.html', context)
+
